bc1d0e4db07b8d52cbe131317a2116114829e81a
vijayshankar037 <vijayshankar1990@hotmail.com>
2020-04-17 19:55:22 +0530

    enables declaration, sourceMap,  outDir option

---
+++ build/server.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var port = process.env.port || 1337;
+var app = express();
+app.listen(port);
+app.use(express.static('public'));
+console.log("Listining on port " + port);
+//# sourceMappingURL=server.js.map
